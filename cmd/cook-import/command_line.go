c88e31c632544e403d2fdd6dcab6b8212fe3671d
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2023-04-05 00:25:47 -0700

    feat: update
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

---
+++ cmd/cook-import/command_line.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var version string
+
+func possibleLogLevels() []string {
+	levels := make([]string, 0)
+
+	for _, l := range log.AllLevels {
+		levels = append(levels, l.String())
+	}
+
+	return levels
+}
+
+func initializeCli() {
+	logLevelName := viper.GetString("log-level")
+	logLevel, err := log.ParseLevel(logLevelName)
+	if err != nil {
+		log.Errorf("Failed to parse provided log level %s: %s", logLevelName, err)
+		os.Exit(1)
+	}
+
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	log.SetLevel(logLevel)
+}
+
+func newCookImportCommand(run func(cmd *cobra.Command, args []string)) (*cobra.Command, error) {
+	command := &cobra.Command{
+		Use:     "cook-import",
+		Short:   "cook-import is a command line tool to import recipes into Cooklang format",
+		Version: version,
+		Run:     run,
+	}
+	logLevelUsage := fmt.Sprintf("level of logs that should printed, one of (%s)", strings.Join(possibleLogLevels(), ", "))
+	command.PersistentFlags().BoolP("dry-run", "d", false, "don't actually render any markdown files just print to stdout passed")
+	command.PersistentFlags().StringP("log-level", "L", "info", logLevelUsage)
+	command.PersistentFlags().StringP("openai-api-key", "k", "", "OpenAI API key")
+	command.PersistentFlags().StringP("link", "l", "", "Link to use")
+	command.MarkPersistentFlagRequired("link")
+	
+	viper.SetConfigName(".cookimport")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("/etc/cook-import/")
+	viper.AddConfigPath("$HOME/.config/")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Debug(err)
+		} else {
+			log.Debugf("Error occurred while reading config file: %s \n", err)
+		}
+	} else {
+		log.Debugf("Using config file %s", viper.ConfigFileUsed())
+	}
+
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("COOK_IMPORT")
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.RegisterAlias("dryRun", "dry-run")
+	viper.RegisterAlias("logLevel", "log-level")
+	viper.RegisterAlias("openAiApiKey", "openai-api-key")
+	err = viper.BindPFlags(command.PersistentFlags())
+	return command, err
+}
--- cmd/cook-import/main.go
+++ cmd/cook-import/main.go
@@ -1,29 +1,143 @@
 package main
 
 import (
-	"fmt"
+	"regexp"
+	"os"
 	log "github.com/sirupsen/logrus"
-	"github.com/solywsh/chatgpt"
-	"time"
+	openai "github.com/sashabaranov/go-openai"
+	"context"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"net/url"
 )
 
-func main() {
-		// https://pkg.go.dev/github.com/solywsh/chatgpt
-		// The timeout is used to control the situation that the session is in a long and multi session situation.
-		// If it is set to 0, there will be no timeout. Note that a single request still has a timeout setting of 30s.
-		chat := chatgpt.New("openai_key", "user_id(not required)", 30*time.Second)
-		defer chat.Close()
-		//
-		//select {
-		//case <-chat.GetDoneChan():
-		//	fmt.Println("time out/finish")
-		//}
-		question := "你认为2022年世界杯的冠军是谁？"
-		fmt.Printf("Q: %s\n", question)
-		answer, err := chat.Chat(question)
+const defaultMessage = `Here are the specification for a markup language called cooklang, used to describe a cooking recipe:
+Define an ingredient using the @ symbol, indicate the end of the ingredient's name with {}, indicate the quantity of an item inside {} after the name,
+indicate the unit of an item's quantity, such as weight or volume, using %, define any necessary cookware with #, define a timer using ~
+Here is an example:
+` +
+"```" +
+`
+Crack the @eggs{3} into a blender, then add the @flour{125%g), @milk{250%ml} and @sea salt{1%pinch}, and blitz until smooth.
+
+Pour into a #bowl and leave to stand for ~{15%minutes}.
+
+Melt the @butter{} (or a drizzle of @oil if you want to be a bit healthier) in a #large non-stick frying pan{} on a medium heat, then tilt the pan so the butter coats the surface.
+
+Pour in 1 ladle of batter and tilt again, so that the batter spreads all over the base, then cook for 1 to 2 minutes, or until it starts to come away from the sides.
+
+Once golden underneath, flip the pancake over and cook for 1 further minute, or until cooked through.
+
+Serve straightaway with your favorite topping. - Add your favorite topping here to make sure it's included in your meal plan!
+`+
+"```" +
+`
+Never make the list of ingredients, only write the instructions using the markup
+
+Abbreviate ingredient measurements and convert fractions to decimals in ingredients
+
+Using cooklang, get the recipe instructions at the link` // https://healthyrecipesblogs.com/crustless-quiche/
+
+func getResponse(message string, c *openai.Client) (resp openai.ChatCompletionResponse, err error) {
+	return
+}
+
+func cookImport(_ *cobra.Command, _ []string) {
+	initializeCli()
+	link := viper.GetString("link")
+	_, err := url.ParseRequestURI(link)
+
+	if err != nil {
+		log.Errorf("Invalid URI: %v\n", err)
+		return
+	}
+	
+	log.Debugf("link: %s\n", link)
+	message := defaultMessage + " " + link
+	log.Debugf("message: %s\n", message)
+	key := viper.GetString("openai-api-key")
+	log.Debugf("key: %s\n", key)
+	if len(key) == 0 {
+		log.Errorln("OpenAI API key is not set")
+		return
+	} 
+	client := openai.NewClient(key)
+	ctx := context.Background() 
+	model := openai.GPT3Dot5Turbo
+	
+	messages := []openai.ChatCompletionMessage{
+												{
+													Role:    openai.ChatMessageRoleUser,
+													Content: message,
+												},
+											}
+	resp, err := client.CreateChatCompletion(
+								ctx,
+								openai.ChatCompletionRequest{
+										Model: model,
+										Messages: messages,
+								},
+						)
+	if err != nil {
+		log.Errorf("ChatCompletion error: %v\n", err)
+		return
+	}
+	content := resp.Choices[0].Message.Content
+
+	messages = []openai.ChatCompletionMessage{
+												{
+													Role:    openai.ChatMessageRoleUser,
+													Content: "Get the number of servings of the recipe at this link " + link + ". Return only the digit",
+												},
+											}
+											
+	resp, err = client.CreateChatCompletion(
+							ctx,
+							openai.ChatCompletionRequest{
+										Model: model,
+										Messages: messages,
+								},
+						)
 		if err != nil {
-			fmt.Println(err)
+			log.Errorf("ChatCompletion error: %v\n", err)
+			return
 		}
-		fmt.Printf("A: %s\n", answer)
-		log.Info("test")
+	re := regexp.MustCompile("[0-9]+")
+	servings := re.FindString(resp.Choices[0].Message.Content)
+
+	messages = []openai.ChatCompletionMessage{
+													{
+														Role:    openai.ChatMessageRoleUser,
+														Content: "Gget the title of the recipe from the link " + link,
+													},
+												}
+												
+		resp, err = client.CreateChatCompletion(
+								ctx,
+								openai.ChatCompletionRequest{
+											Model: model,
+											Messages: messages,
+									},
+							)
+			if err != nil {
+				log.Errorf("ChatCompletion error: %v\n", err)
+				return
+			}
+	title := resp.Choices[0].Message.Content
+	content = `>> source: ` + link + "\n" + ">> serves: " + servings + "\n\n" + content
+	log.Infoln(content)
+	log.Infoln(title)
+}
+
+func main() {
+	command, err := newCookImportCommand(cookImport)
+	if err != nil {
+		log.Errorf("Failed to create the CLI commander: %s", err)
+		os.Exit(1)
+	}
+	
+	if err := command.Execute(); err != nil {
+		log.Errorf("Failed to start the CLI: %s", err)
+		os.Exit(1)
+	}
 }
